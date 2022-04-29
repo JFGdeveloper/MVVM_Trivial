8aa28df1296dcbae937c4bef20e34175a0fabee3
Javier fernandez <javierfernandezdeveloper@gmail.com>
2022-04-29 02:13:25 +0200

    Añadimos la linea

--- app/src/main/java/com/javidev/trivial/component/Question.kt
+++ app/src/main/java/com/javidev/trivial/component/Question.kt
@@ -1,17 +1,17 @@
 package com.javidev.trivial.component
 
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -44,14 +44,17 @@ fun Question(viewModel: QuestionViewModel) {
     }
 }
 
-
+@Preview
 @Composable
 fun QuestionDisplay() {
+
+    // esto es lo que le da la discontinuidad a la linea
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
     Surface(
         modifier = Modifier
             .fillMaxSize()
             .padding(4.dp),
-            color = Colors.mDarkPurple // he creado mis propios colores en un objeto en pack util
+        color = Colors.mDarkPurple // he creado mis propios colores en un objeto en pack util
     ) {
         Column(
             modifier = Modifier.padding(4.dp),
@@ -60,6 +63,7 @@ fun QuestionDisplay() {
         ) {
 
             QuestionTracker()
+            DrawDottedLine(pathEffect = pathEffect)
 
         }
 
@@ -68,24 +72,51 @@ fun QuestionDisplay() {
 
 @Preview
 @Composable
-fun QuestionTracker(counter: Int = 10,
-                    outOf: Int = 100) {
-    Text(text = buildAnnotatedString {
-        withStyle(style = ParagraphStyle(textIndent = TextIndent.None)){
-            withStyle(style = SpanStyle(color = Colors.mLightGray,
-                fontWeight = FontWeight.Bold,
-                fontSize = 27.sp)
-            ){
-                append("Question $counter/")
-                withStyle(style = SpanStyle(color = Colors.mLightGray,
-                    fontWeight = FontWeight.Light,
-                    fontSize = 14.sp)){
-                    append("$outOf")
-                }
+fun QuestionTracker(counter: Int = 10, outOf: Int = 100) {
+    Text(
+        text = buildAnnotatedString {
+            withStyle(style = ParagraphStyle(textIndent = TextIndent.None)) {
+                withStyle(
+                    style = SpanStyle(
+                        color = Colors.mLightGray,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 27.sp
+                    )
+                ) {
+                    append("Question $counter/")
+                    withStyle(
+                        style = SpanStyle(
+                            color = Colors.mLightGray,
+                            fontWeight = FontWeight.Light,
+                            fontSize = 14.sp
+                        )
+                    ) {
+                        append("$outOf")
+                    }
 
+                }
             }
-        }
-    },
-        modifier = Modifier.padding(20.dp))
+        },
+        modifier = Modifier.padding(20.dp)
+    )
+
+}
+
+
+@Composable
+fun DrawDottedLine(pathEffect: PathEffect) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(5.dp),
+    ) {
+        drawLine(
+            color = Colors.mLightGray,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, y = 0f),
+            pathEffect = pathEffect
+        )
+
+    }
 
 }
